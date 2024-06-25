b2d66422409a78905aff880ef8c43444106c9e63
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-06-25 10:43:46 -0500

    [PatMat] Fix few crashes and update more match usage
    We can now build `match` expressions for most part of things that used to work
    with `inspect`.
    
    This has some broken testcases and now inspect doesn't work anymore so tests
    will fail. Next steps:
    
    - Remove inspect overall (including tests)
    - Finish clang/test/AST/ast-dump-match-stmt.cpp
    - Remove curly braces from pattern actions.

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -8326,7 +8326,7 @@ ExprResult Sema::ActOnFinishInspectExpr(SourceLocation InspectLoc,
   assert(IE == getCurFunction()->InspectStack.back().getPointer() &&
          "inspect stack missing push/pop!");
 
-  getCurFunction()->InspectStack.pop_back();
+  getCurFunction()->MatchStack.pop_back();
 
   if (!BodyStmt)
     return ExprError();
@@ -8462,7 +8462,7 @@ ExprResult Sema::ActOnFinishMatchExpr(SourceLocation InspectLoc, Expr *Inspect,
   // Expr *CondExpr = IE->getCond();
   // if (!CondExpr)
   //   return ExprError();
-  llvm_unreachable("NYI");
+  // llvm_unreachable("NYI");
 
   const PatternStmt *P = IE->getPatternList();
 
