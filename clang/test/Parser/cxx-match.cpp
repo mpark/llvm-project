d402c033e0da00da0444de7c7e489d7e233d8398
Michael Park <mcypark@gmail.com>
2024-08-28 01:18:17 -0700

    Print the MatchSelectExpr subject and add precedence tests.

--- clang/test/Parser/cxx-match.cpp
+++ clang/test/Parser/cxx-match.cpp
@@ -20,6 +20,27 @@ void test_match_no_rhs(int i) {
   42 match -> void; // expected-error {{expected '{'}}
 }
 
+void test_precedence(int* p) {
+  // unary is tighter than match
+  *p match { _ => 0; };
+  *p match { _ => 0; } + 1;
+  // match binds tighter than bin ops.
+  4 + 2 match { _ => 0; };
+  4 * 2 match { _ => 0; };
+  4 == 2 match { _ => 0; };
+  4 * (2) match { _ => 0; };
+  2 match { _ => 0; } + 1;
+  2 match { _ => 0; } * 1;
+  2 match { _ => 0; } == 1;
+  (2) match { _ => 0; } * 1;
+  // except .* and ->*
+  struct S { int i; } s;
+  s.*&S::i match { _ => 0; };
+  &s->*&S::i match { _ => 0; };
+  2 match { _ => s; } .* &S::i;
+  2 match { _ => &s; } ->* &S::i;
+}
+
 void test_structures(int x) {
   x match { _ => 0; };
   x match { _ if true => 0; };
@@ -37,6 +58,7 @@ void test_structures(int x) {
   x match constexpr -> int { _ if true => 0; };
   x match constexpr -> auto { _ if true => 0; };
   x match constexpr -> decltype(auto) { _ if true => 0; };
+  x match { ? _ => 0; _ => 1; };
 }
 
 void test_wildcard_pattern(int x) {
