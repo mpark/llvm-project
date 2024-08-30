2b8311073c03ba453b955d7bd2af81bf39236eb9
Michael Park <mcypark@gmail.com>
2024-08-29 20:13:26 -0700

    Add support for structured bindings pattern.

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -4497,6 +4497,7 @@ private:
   bool ParseMatchBody(SmallVectorImpl<MatchCase> &Result, SourceRange &Braces);
   bool ParseMatchCase(MatchCase &Case);
   ActionResult<MatchPattern *> ParsePattern(ExprResult *LHS = nullptr);
+  ActionResult<MatchPattern *> ParseDecompositionPattern();
 
   ///@}
 
--- clang/lib/Parse/ParseExprCXX.cpp
+++ clang/lib/Parse/ParseExprCXX.cpp
@@ -3960,6 +3960,7 @@ bool Parser::ParseMatchBody(SmallVectorImpl<MatchCase> &Result, SourceRange& Bra
   while (Tok.isNot(tok::r_brace) && Tok.isNot(tok::eof)) {
     MatchCase Case;
     if (ParseMatchCase(Case)) {
+      T.skipToEnd();
       return true;
     }
     Result.push_back(Case);
@@ -4003,6 +4004,8 @@ ActionResult<MatchPattern *> Parser::ParsePattern(ExprResult *LHS) {
       }
       return Actions.ActOnOptionalPattern(QuestionLoc, Pattern.get());
     }
+    case tok::l_square:
+      return ParseDecompositionPattern();
     case tok::identifier: {
       IdentifierInfo *II = Tok.getIdentifierInfo();
       if (II == Ident_wildcard) {
@@ -4016,7 +4019,7 @@ ActionResult<MatchPattern *> Parser::ParsePattern(ExprResult *LHS) {
     default: {
       ExprResult Expr = [=] {
         if (!LHS) {
-          return ParseExpression();
+          return ParseConstantExpression();
         }
         bool RHSIsInitList = false;
         prec::Level NextTokPrec;
@@ -4035,4 +4038,24 @@ ActionResult<MatchPattern *> Parser::ParsePattern(ExprResult *LHS) {
       return Actions.ActOnExpressionPattern(Expr.get());
     }
   }
+}
+
+ActionResult<MatchPattern *> Parser::ParseDecompositionPattern() {
+  assert(Tok.is(tok::l_square) && "Not a decomposition pattern");
+  BalancedDelimiterTracker T(*this, tok::l_square);
+  if (T.expectAndConsume())
+    return true;
+
+  SmallVector<MatchPattern *, 4> Patterns;
+  do {
+    ActionResult<MatchPattern *> Pattern = ParsePattern();
+    if (Pattern.isInvalid()) {
+      T.skipToEnd();
+      return true;
+    }
+    Patterns.push_back(Pattern.get());
+  } while (TryConsumeToken(tok::comma));
+  T.consumeClose();
+
+  return Actions.ActOnDecompositionPattern(Patterns, T.getRange());
 }
