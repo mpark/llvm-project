24d465cfcbc71eb6e77b5507aebfb5c9a820e4bc
Michael Park <mcypark@gmail.com>
2024-08-30 02:04:35 -0700

    Start adding support for binding patterns.

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -4498,7 +4498,8 @@ private:
   bool ParseMatchBody(SmallVectorImpl<MatchCase> &Result, SourceRange &Braces);
   bool ParseMatchCase(MatchCase &Case);
   ActionResult<MatchPattern *> ParsePattern(ExprResult *LHS = nullptr);
-  ActionResult<MatchPattern *> ParseDecompositionPattern();
+  ActionResult<MatchPattern *> ParseBindingPattern();
+  ActionResult<MatchPattern *> ParseDecompositionPattern(bool BindingOnly);
 
   ///@}
 
