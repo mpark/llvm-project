8587573b21a2d62c3f5e6366bb0c0eab86151fbe
Michael Park <mcypark@gmail.com>
2024-09-10 21:36:22 -0700

    Add expr classification of match test expr such that it can be used with a deduced return type.

--- clang/lib/AST/ExprClassification.cpp
+++ clang/lib/AST/ExprClassification.cpp
@@ -217,6 +217,7 @@ static Cl::Kinds ClassifyInternal(ASTContext &Ctx, const Expr *E) {
   case Expr::SourceLocExprClass:
   case Expr::ConceptSpecializationExprClass:
   case Expr::RequiresExprClass:
+  case Expr::MatchTestExprClass:
     return Cl::CL_PRValue;
 
   case Expr::EmbedExprClass:
