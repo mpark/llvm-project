8e80a08fe33fb769a969921974a9aad5339f9b6a
Michael Park <mcypark@gmail.com>
2024-09-10 19:43:58 -0700

    Implement constant evaluation for decomposition decl.

--- clang/lib/Parse/ParseExprCXX.cpp
+++ clang/lib/Parse/ParseExprCXX.cpp
@@ -4114,8 +4114,9 @@ StmtResult Parser::ParseMatchHandler() {
     SemiError = "co_return";
     break;
   default: {
-    ExprResult E =
-        Tok.is(tok::l_brace) ? ParseInitializer() : ParseExpression();
+    ExprResult E = Tok.is(tok::l_brace)
+                       ? ParseInitializer()
+                       : Actions.ActOnStmtExprResult(ParseExpression());
     Result = E.isInvalid() ? StmtError() : StmtResult(E.get());
     break;
   }
