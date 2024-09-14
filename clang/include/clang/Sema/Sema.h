af2c3beb473b02f6a333e87203b9e9355d43ea9b
Michael Park <mcypark@gmail.com>
2024-09-14 16:36:46 -0700

    Implement parenthesized patterns.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -11244,6 +11244,7 @@ public:
   ExprResult ActOnMatchSubject(Expr *Subject);
   StmtResult ActOnMatchExprHandler(TypeLoc OrigResultType, QualType &RetTy,
                                    ExprResult ER);
+
   ExprResult ActOnMatchTestExpr(Expr *Subject, SourceLocation MatchLoc,
                                 MatchPattern *Pattern);
   ExprResult ActOnMatchSelectExpr(Expr *Subject, SourceLocation MatchLoc,
@@ -11253,8 +11254,11 @@ public:
 
   ActionResult<MatchPattern *> ActOnWildcardPattern(SourceLocation WildcardLoc);
   ActionResult<MatchPattern *> ActOnExpressionPattern(Expr *SubExpr);
-  ActionResult<MatchPattern *> ActOnBindingPattern(SourceLocation NameLoc,
+  ActionResult<MatchPattern *> ActOnBindingPattern(SourceLocation LetLoc,
+                                                   SourceLocation NameLoc,
                                                    IdentifierInfo *Name);
+  ActionResult<MatchPattern *> ActOnParenPattern(SourceRange Parens,
+                                                 MatchPattern *SubPattern);
   ActionResult<MatchPattern *> ActOnOptionalPattern(SourceLocation QuestionLoc,
                                                     MatchPattern *SubPattern);
   ActionResult<MatchPattern *>
