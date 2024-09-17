7bead4dadfe4eea358f117571a260f473a6675c4
Michael Park <mcypark@gmail.com>
2024-09-17 06:42:18 -0700

    Implement pattern guards.

--- clang/include/clang/AST/ASTNodeTraverser.h
+++ clang/include/clang/AST/ASTNodeTraverser.h
@@ -1041,6 +1041,9 @@ public:
   void VisitMatchTestExpr(const MatchTestExpr *Node) {
     Visit(Node->getSubject());
     VisitMatchPattern(Node->getPattern());
+    const Expr *Guard = Node->getGuard();
+    if (Guard)
+      Visit(Guard);
   }
 
   void VisitMatchSelectExpr(const MatchSelectExpr *Node) {
