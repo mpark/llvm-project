5b4fff9312224415de45481c07dd832c526fd298
Zach Laine <whatwasthataddress@gmail.com>
2024-12-31 14:28:13 -0600

    Add pattern matching support to std::expected, following P3527.

---
+++ libcxx/test/std/utilities/expected/expected.get/get_if_index.pass.cpp
@@ -0,0 +1,117 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, c++20, c++23
+
+// <expected>
+
+// template <size_t I, class T, class E>
+//  constexpr add_pointer_t<variant_alternative_t<I, variant<Types...>>>
+//   get_if(expected<T, E>* e) noexcept;
+// template <size_t I, class T, class E>
+//  constexpr add_pointer_t<const variant_alternative_t<I, variant<Types...>>>
+//   get_if(const expected<T, E>* e) noexcept;
+
+#include "test_macros.h"
+#include "variant_test_helpers.h"
+#include <cassert>
+#include <memory>
+#include <expected>
+
+void test_const_get_if() {
+  {
+    using E = std::expected<int, long>;
+    constexpr const E *e = nullptr;
+    static_assert(std::get_if<0>(e) == nullptr, "");
+  }
+  {
+    using E = std::expected<int, long>;
+    constexpr E e(42);
+    ASSERT_NOEXCEPT(std::get_if<0>(&e));
+    ASSERT_SAME_TYPE(decltype(std::get_if<0>(&e)), const int *);
+    static_assert(*std::get_if<0>(&e) == 42, "");
+    static_assert(std::get_if<1>(&e) == nullptr, "");
+  }
+  {
+    using E = std::expected<int, long>;
+    constexpr E e(std::unexpect, 42l);
+    ASSERT_SAME_TYPE(decltype(std::get_if<1>(&e)), const long *);
+    static_assert(*std::get_if<1>(&e) == 42, "");
+    static_assert(std::get_if<0>(&e) == nullptr, "");
+  }
+  {
+    using E = std::expected<int, int>;
+    constexpr const E *e = nullptr;
+    static_assert(std::get_if<0>(e) == nullptr, "");
+  }
+  {
+    using E = std::expected<int, int>;
+    constexpr E e(42);
+    ASSERT_NOEXCEPT(std::get_if<0>(&e));
+    ASSERT_SAME_TYPE(decltype(std::get_if<0>(&e)), const int *);
+    static_assert(*std::get_if<0>(&e) == 42, "");
+    static_assert(std::get_if<1>(&e) == nullptr, "");
+  }
+  {
+    using E = std::expected<int, int>;
+    constexpr E e(std::unexpect, 42l);
+    ASSERT_SAME_TYPE(decltype(std::get_if<1>(&e)), const int *);
+    static_assert(*std::get_if<1>(&e) == 42, "");
+    static_assert(std::get_if<0>(&e) == nullptr, "");
+  }
+}
+
+void test_get_if() {
+  {
+    using E = std::expected<int, long>;
+    E *e = nullptr;
+    assert(std::get_if<0>(e) == nullptr);
+  }
+  {
+    using E = std::expected<int, long>;
+    E e(42);
+    ASSERT_NOEXCEPT(std::get_if<0>(&e));
+    ASSERT_SAME_TYPE(decltype(std::get_if<0>(&e)), int *);
+    assert(*std::get_if<0>(&e) == 42);
+    assert(std::get_if<1>(&e) == nullptr);
+  }
+  {
+    using E = std::expected<int, long>;
+    E e(std::unexpect, 42l);
+    ASSERT_SAME_TYPE(decltype(std::get_if<1>(&e)), long *);
+    assert(*std::get_if<1>(&e) == 42);
+    assert(std::get_if<0>(&e) == nullptr);
+  }
+  {
+    using E = std::expected<int, int>;
+    E *e = nullptr;
+    assert(std::get_if<0>(e) == nullptr);
+  }
+  {
+    using E = std::expected<int, int>;
+    E e(42);
+    ASSERT_NOEXCEPT(std::get_if<0>(&e));
+    ASSERT_SAME_TYPE(decltype(std::get_if<0>(&e)), int *);
+    assert(*std::get_if<0>(&e) == 42);
+    assert(std::get_if<1>(&e) == nullptr);
+  }
+  {
+    using E = std::expected<int, int>;
+    E e(std::unexpect, 42l);
+    ASSERT_SAME_TYPE(decltype(std::get_if<1>(&e)), int *);
+    assert(*std::get_if<1>(&e) == 42);
+    assert(std::get_if<0>(&e) == nullptr);
+  }
+}
+
+int main(int, char**) {
+  test_const_get_if();
+  test_get_if();
+
+  return 0;
+}
