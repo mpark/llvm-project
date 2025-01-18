7f686978b448cda3c6b1716003011376a80db956
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2025-01-17 18:43:33 -0800

    [PatMat][CodeGen][NFC] Abstract pattern emission into its own method

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -5349,6 +5349,7 @@ public:
 
   void EmitFakeUse(Address Addr);
   RValue EmitMatchTestExpr(const MatchTestExpr &S);
+  RValue EmitMatchPattern(const MatchPattern *Pattern, const Expr *Subject);
 
   //===--------------------------------------------------------------------===//
   //                         Annotations Emission
