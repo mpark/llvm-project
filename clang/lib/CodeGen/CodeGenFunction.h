cf6044fb9cab2581a300823a0eefc4483bb0cc18
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2025-01-30 13:18:09 -0800

    [PatMat][CodeGen] Support Alternative Pattern

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -5352,6 +5352,7 @@ public:
   RValue EmitMatchSelectExpr(const MatchSelectExpr &S);
   RValue EmitMatchPattern(const MatchPattern *Pattern, const Expr *Subject);
   RValue EmitDecompositionPattern(const DecompositionPattern *Pattern);
+  RValue EmitAlternativePattern(const AlternativePattern *Pattern);
 
   //===--------------------------------------------------------------------===//
   //                         Annotations Emission
