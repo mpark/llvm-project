7a01f552041d4c57bbdb6a760c8b95bf92b8fbfc
Michael Park <mcypark@gmail.com>
2026-09-04 12:33:33 -0700

    [Clang][PatternMatching] Target C++29
    Require -std=c++2d when enabling pattern matching. Migrate the existing
    pattern-matching tests to C++29 mode and rename the three inherited
    cxx2c-match tests accordingly.
    
    Authored by GPT-5.6 Sol

--- clang/test/CodeGenCXX/PatternMatching/match-expr-agg.cpp
+++ clang/test/CodeGenCXX/PatternMatching/match-expr-agg.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown -fpattern-matching -fcxx-exceptions -Wno-c++20-extensions -O0 -emit-llvm %s -o %t.ll
+// RUN: %clang_cc1 -std=c++2d -triple x86_64-unknown-unknown -fpattern-matching -fcxx-exceptions -Wno-c++20-extensions -O0 -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s
 
 enum Color { Red, Blue };
